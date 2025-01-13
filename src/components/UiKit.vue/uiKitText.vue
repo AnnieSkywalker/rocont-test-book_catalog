119a199d6aa19b19252976ac90d71e33d149f797
spiridonov <annie.skywalker@yandex.ru>
2025-01-13 23:12:28 +0300

    color shema

--- src/components/UiKit.vue/uiKitText.vue
+++ src/components/UiKit.vue/uiKitText.vue
@@ -6,6 +6,14 @@
         <p class="p">p Текст</p>
         <p class="note">note Подпись у форм и чекбокс</p>
     </div>
+
+    <div class="checkbox">
+        <h1>H1 Заголовок</h1>
+        <p class="lead">Lead Название книги</p>
+        <p class="p-bold">p bold Лейбл инпута</p>
+        <p class="p">p Текст</p>
+        <p class="note">note Подпись у форм и чекбокс</p>
+    </div>
 </template>
 
 <script></script>
@@ -20,36 +28,42 @@
         border-radius: 2px 0px 0px 0px;
         border: 1px 0px 0px 0px;
         opacity: 0px;
+        background-color: var(--white);
     }
 
     h1 {
         font-size: 36px;
         font-weight: 600;
         line-height: 36px;
+        color: var(--text-title);
         text-align: left;
     }
     .lead {
         font-size: 24px;
         font-weight: 400;
         line-height: 24px;
+        color: var(--text-title);
         text-align: left;
     }
     .p-bold {
         font-size: 16px;
         font-weight: 600;
         line-height: 24px;
+        color: var(--text-title);
         text-align: left;
     }
     .p {
         font-size: 16px;
         font-weight: 400;
         line-height: 24px;
+        color: var(--text);
         text-align: left;
     }
     .note {
         font-size: 14px;
         font-weight: 400;
         line-height: 14px;
+        color: var(--text);
         text-align: left;
     }
 </style>
